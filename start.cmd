0fbf3312a5cb1eb98ec31ae20b3f940bd67d31f4
pcuisinaud <philippe.cuisinaud@gmail.com>
2016-12-06 18:08:55 +0100

    -

--- start.cmd
+++ start.cmd
@@ -1,5 +1,7 @@
-start "Config-Service"    java -Xmx32m -Dhttps.proxyHost=vipproxy1.prod.extelia.fr -Dhttps.proxyPort=8080 -jar config-server\target\config-server.jar
-start "Eureka-Service"    java -Xmx256m -jar eureka-server\target\eureka-server.jar
-start                     java -Xmx512m -jar reservation-service\target\reservation-service.jar
-start                     java -Xmx256m -jar reservation-client\target\reservation-client.jar
-start "Hystrix-Dashboard" java -Xmx256m -jar hystrix-dashboard\target\hystrix-dashboard.jar
+start "Config-Service"      java -Xmx32m -Dhttps.proxyHost=vipproxy1.prod.extelia.fr -Dhttps.proxyPort=8080 -jar config-server\target\config-server.jar
+pause
+start "Eureka-Service"      java -Xmx256m -jar eureka-server\target\eureka-server.jar
+pause
+start "Reservation-Service" java -Xmx512m -jar reservation-service\target\reservation-service.jar
+start "Reservation-Client"  java -Xmx256m -jar reservation-client\target\reservation-client.jar
+start "Hystrix-Dashboard"   java -Xmx256m -jar hystrix-dashboard\target\hystrix-dashboard.jar
